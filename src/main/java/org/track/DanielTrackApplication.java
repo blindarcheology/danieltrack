ea0e50ea7e8fffc58364193f1fea4164c0fdf083
lijiuyi <lijiuyi@tinman.cn>
2020-09-24 23:12:05 +0800

    初始化项目 & 执行验证监控端点可行性

---
+++ src/main/java/org/track/DanielTrackApplication.java
@@ -0,0 +1,14 @@
+package org.track;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author blindarcheology@hotmail.com
+ */
+@SpringBootApplication
+public class DanielTrackApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DanielTrackApplication.class,args);
+    }
+}
