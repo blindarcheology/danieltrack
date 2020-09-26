2ac5d4197098c07e16eadb19babeea4874cadcb4
blindarcheology <blindarcheology@hotmail.com>
2020-09-26 15:39:43 +0800

    添加单元测试

---
+++ src/test/java/org/track/bytecode/JunitTests.java
@@ -0,0 +1,15 @@
+package org.track.bytecode;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.track.DanielTrackApplicationTests;
+
+public class JunitTests extends DanielTrackApplicationTests {
+
+    @Test
+    public void testUnit(){
+        long v = 1L;
+        Assert.assertEquals(1,v);
+    }
+
+}
