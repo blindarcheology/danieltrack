41c07aa1c489fdcf1d3f786352a43c6a927d2351
blindarcheology <blindarcheology@hotmail.com>
2020-09-26 16:09:45 +0800

    修改maven编译插件以及版本

---
+++ src/test/java/org/track/bytecode/ParameterDescriptorTest.java
@@ -0,0 +1,16 @@
+package org.track.bytecode;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.track.DanielTrackApplicationTests;
+
+import java.util.List;
+
+public class ParameterDescriptorTest extends DanielTrackApplicationTests {
+
+    @Test
+    public void testParamDesc(int a, Double b, String[] s, List<String> lists){
+        Assert.assertTrue(true);
+    }
+
+}
