eeb01eb902ea3027bb5f6e9e5eab9c81da36c447
blindarcheology <blindarcheology@hotmail.com>
2020-09-28 10:47:29 +0800

    add MachineCodeTests

---
+++ src/test/java/org/track/machinecode/MachineCodeTests.java
@@ -0,0 +1,31 @@
+package org.track.machinecode;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.track.DanielTrackApplicationTests;
+import oshi.SystemInfo;
+import oshi.hardware.ComputerSystem;
+import oshi.hardware.HardwareAbstractionLayer;
+import oshi.software.os.OperatingSystem;
+
+public class MachineCodeTests extends DanielTrackApplicationTests {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(MachineCodeTests.class);
+
+    @Test
+    public void testMachineCode(){
+        SystemInfo systemInfo = new SystemInfo();
+        OperatingSystem operatingSystem = systemInfo.getOperatingSystem();
+        HardwareAbstractionLayer hardwareAbstractionLayer = systemInfo.getHardware();
+        ComputerSystem computerSystem = hardwareAbstractionLayer.getComputerSystem();
+
+        String vendor = operatingSystem.getManufacturer();
+        String processorSerialNumber = computerSystem.getSerialNumber();
+
+        Assert.assertEquals("Apple",vendor);
+        LOGGER.info("processor serial number is:{}",processorSerialNumber);
+    }
+
+}
