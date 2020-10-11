8ab8f3eda442e29b55ab3c9a8d31e4b855245332
blindarcheology <blindarcheology@hotmail.com>
2020-10-11 18:22:02 +0800

    add test cause

---
+++ src/test/java/org/track/arthas/AlibabaArthasTests.java
@@ -0,0 +1,13 @@
+package org.track.arthas;
+
+import org.junit.Test;
+import org.track.DanielTrackApplicationTests;
+
+public class AlibabaArthasTests extends DanielTrackApplicationTests {
+
+    @Test
+    public void test() {
+
+    }
+
+}
